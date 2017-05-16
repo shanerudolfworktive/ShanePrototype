77b8887aa0198d36b8d47d942dec3de45b7d3f8b
Shane <shane@versame.com>
2017-05-16 16:49:32 -0600

    change app name

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=app/build/outputs/apk/app-debug.apk" \
+-F "ipa=ShanePrototype/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: 4bcd149148c3120b3b94951e4fdc59ad" \
 https://rink.hockeyapp.net/api/2/apps/5d02926c41e848588f31c3b62969782d/app_versions/upload
