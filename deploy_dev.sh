78336ad8783f279d1a9d8e58f171b7cb0eb7085f
Shane <shane@versame.com>
2017-05-16 17:09:03 -0600

    update tocken

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -2,5 +2,5 @@ curl \
 -F "status=2" \
 -F "notify=0" \
 -F "ipa=ShanePrototype/build/outputs/apk/app-debug.apk" \
--H "X-HockeyAppToken: 4bcd149148c3120b3b94951e4fdc59ad" \
+-H "X-HockeyAppToken: 44dc3597af6b41379d8995f9abab4968" \
 https://rink.hockeyapp.net/api/2/apps/5d02926c41e848588f31c3b62969782d/app_versions/upload
